9fdbd01e470ec62584a9ddddb2bbbced9431789a
marriola <matt.arriola87@gmail.com>
2017-05-18 01:33:00 -0400

    Merging for realsies

--- src/elm/Decoders/Conditions.elm
+++ src/elm/Decoders/Conditions.elm
@@ -1,11 +1,15 @@
-module Decoders.Conditions exposing (WeatherResponse, WeatherResponseMain, Conditions, SearchResult, WeatherError, weatherResponse)
+module Decoders.Conditions exposing (Conditions, currentObservation)
+
+import Decoders.DisplayLocation exposing (DisplayLocation, displayLocation)
 
 import Json.Decode exposing (Decoder, field, oneOf, float, int, list, string, map, maybe, map2, map3, map4, map5, null, oneOf)
 import Json.Decode.Pipeline exposing (decode, required, optional)
 
+
 maybeFloat : Decoder (Maybe Float)
 maybeFloat =
   map (\x -> Just x) float
+
 
 -- takes a string and converts it into a Maybe Int. the Wunderground API returns
 -- "NA" instead of null for some values, these are parsed to Nothing.
@@ -13,113 +17,45 @@ stringToFloatDecoder : Decoder (Maybe Float)
 stringToFloatDecoder =
   map (\x -> String.toFloat x |> Result.toMaybe) string
 
-type alias SearchResult = {
-  name : String,
-  city : String,
-  state : String,
-  country : String,
-  country_iso3166 : String,
-  country_name : String,
-  zmw : String,
-  l : String
-}
 
-type alias Conditions = {
-  key : Int, -- set by ContentPanel
+type alias Features =
+    { conditions : Int
+    }
 
-  display_location : DisplayLocation,
-  weather : String,
-  temp_f : Float,
-  temp_c : Float,
-  relative_humidity : String,
-  wind_dir : String,
-  wind_mph : Float,
-  wind_kph : Float,
-  pressure_mb : String,
-  dewpoint_f : Float,
-  dewpoint_c : Float,
-  heat_index_f : Maybe Float,
-  heat_index_c : Maybe Float,
-  windchill_f : Maybe Float,
-  windchill_c : Maybe Float,
-  feelslike_f : Maybe Float,
-  feelslike_c : Maybe Float,
-  visibility_mi : String,
-  visibility_km : String,
-  uv : String,
-  precip_today_in : String,
-  precip_today_metric : String
-}
 
-type alias DisplayLocation = {
-  full : String,
-  city : String,
-  state : String,
-  state_name : String,
-  country : String,
-  country_iso3166 : String,
-  zip : String,
-  magic : String,
-  wmo : String,
-  latitude : String,
-  longitude : String,
-  elevation : String
-}
+type alias Conditions =
+    { key : Int -- set by ContentPanel
 
-type alias Features = {
-  conditions : Int
-}
+    , display_location : DisplayLocation
+    , weather : String
+    , temp_f : Float
+    , temp_c : Float
+    , relative_humidity : String
+    , wind_dir : String
+    , wind_mph : Float
+    , wind_kph : Float
+    , pressure_mb : String
+    , dewpoint_f : Float
+    , dewpoint_c : Float
+    , heat_index_f : Maybe Float
+    , heat_index_c : Maybe Float
+    , windchill_f : Maybe Float
+    , windchill_c : Maybe Float
+    , feelslike_f : Maybe Float
+    , feelslike_c : Maybe Float
+    , visibility_mi : String
+    , visibility_km : String
+    , uv : String
+    , precip_today_in : String
+    , precip_today_metric : String
+    }
 
-type alias WeatherError = {
-  typePrime : String,
-  description : String
-}
-
-type alias WeatherResponseMain = {
-  version : String,
-  termsofService : String,
-  features : Features,
-  results : Maybe (List SearchResult),
-  error : Maybe WeatherError
-}
-
-type alias WeatherResponse = {
-  response : WeatherResponseMain,
-  current_observation : Maybe Conditions
-}
 
 features : Decoder Features
 features =
   decode Features
     |> optional "conditions" int 0
 
-searchResult : Decoder SearchResult
-searchResult =
-  decode SearchResult
-    |> required "name" string
-    |> required "city" string
-    |> required "state" string
-    |> required "country" string
-    |> required "country_iso3166" string
-    |> required "country_name" string
-    |> required "zmw" string
-    |> required "l" string
-
-displayLocation : Decoder DisplayLocation
-displayLocation =
-  decode DisplayLocation
-    |> required "full" string
-    |> required "city" string
-    |> required "state" string
-    |> required "state_name" string
-    |> required "country" string
-    |> required "country_iso3166" string
-    |> required "zip" string
-    |> required "magic" string
-    |> required "wmo" string
-    |> required "latitude" string
-    |> required "longitude" string
-    |> required "elevation" string
 
 currentObservation : Decoder Conditions
 currentObservation =
@@ -150,27 +86,3 @@ currentObservation =
       |> required "UV" string
       |> required "precip_today_in" string
       |> required "precip_today_metric" string
-
-decodeConditions : Decoder Conditions
-decodeConditions = field "current_observation" currentObservation
-
-weatherError : Decoder WeatherError
-weatherError =
-  decode WeatherError
-    |> required "type" string
-    |> required "description" string
-
-weatherResponseMain : Decoder WeatherResponseMain
-weatherResponseMain =
-  map5 WeatherResponseMain
-    (field "version" string)
-    (field "termsofService" string)
-    (field "features" features)
-    (maybe (field "results" (list searchResult)))
-    (maybe (field "error" weatherError))
-
-weatherResponse : Decoder WeatherResponse
-weatherResponse =
-  map2 WeatherResponse
-    (field "response" weatherResponseMain)
-    (maybe (field "current_observation" currentObservation))
